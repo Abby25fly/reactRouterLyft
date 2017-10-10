04cf7967eb528e0c9614eff07d7ea8413547a6e4
jais210 <jakeline_210@hotmail.com>
2017-10-10 10:05:44 -0500

    inicio

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
+      <form className="demoForm">
+       <h2>Sign up</h2>
+       <div className="form-group">
+         <label htmlFor="email">Email address</label>
+         <input type="email" className="form-control"
+           name="email" />
+       </div>
+       <div className="form-group">
+         <label htmlFor="password">Password</label>
+         <input type="password" className="form-control"
+           name="password" />
+       </div>
+       <button type="submit" className="btn btn-primary">
+          Sign up
+       </button>
+     </form>
+    
       <div>
         <header>
           <div className="container">
