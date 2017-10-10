f36abc76806bd355e429d184a74c9bad0e074d0f
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-10-10 15:32:58 -0700

    form

--- src/SignUP_profile.js
+++ src/SignUP_profile.js
@@ -14,11 +14,19 @@ class SignUP_profile extends Component {
 		super (props);
 		this.state = {
 			goFordward : false
-		}
+        }
+        
+        this.userInfo = {
+            name:null,
+            surname:null,
+            email:null,
+            phone:null
+        }
 	}
 
 	render () {
 		const {model} = this.props;
+		console.log('SignUpForm');
 
 		const onInputChange = (e) => {
 			this.setState ({
@@ -28,74 +36,66 @@ class SignUP_profile extends Component {
 
 		return (
 			<div>
-                <header>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-1 col-xs-1 text-center">
-                                <a href="javascript:window.history.back();">
-                                    <i
-                                    className="fa fa-angle-left fa-3x"
-                                    aria-hidden="true"/>
-                                </a>
-                            </div>
-                            <div className="col-md-10 col-xs-10 text-center">
-                                <h1>Sign Up</h1>
-                                <br />
-                                <p>Join now for free ride credit</p>
-                            </div>
-                        </div>
+				<header>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-1 col-xs-1 text-center">
+                      <a href="javascript:window.history.back();">
+                      <i
+                      className="fa fa-chevron-left arrow"
+                      aria-hidden="true" />
+                      </a>
                     </div>
-                </header>
-                <div>
-                    <form id="registrationForm" className="form-horizontal">
-                        <div className="form-group">
-                            <i className="fa fa-user-circle-o col-xs-3" aria-hidden="true" />
-                            <div className="col-xs-4">
+                    <div className="col-md-10 col-xs-10 text-center">
+                      <h1>Sign Up</h1>
+                    </div>
+                  </div>
+                </div>
+              </header>
+				<section className="container-fluid form">
+                    <div>
+                        <center>
+                        <form id="registrationForm" className="input-group">
+                        <div className="form-group dataRegister">
+                            <i className="fa fa-user-circle-o col-xs-2" aria-hidden="true" />
+                            <div className="col-md-5 col-xs-5 dataInput">
                                 <label htmlFor="text">Fisrt Name</label>
                                 <input type="text" className="form-control" name="fist name" id="fName" placeholder="First name" />
                             </div>
-                            <div className="col-xs-4">
+                            <div className="col-md-5 col-xs-5 dataInput">
                                 <label htmlFor="text">Last Name</label>
                                 <input type="text" className="form-control" name="last name" id="lName" placeholder="Last name" />
                             </div>
                         </div>
-                        <div className="form-group">
-                            <i className="fa fa-envelope col-xs-3 " aria-hidden="true" />
-                            <div className="col-xs-4">
+                        <div className="form-group dataRegister">
+                            <i className="fa fa-envelope col-xs-2 " aria-hidden="true" />
+                            <div className="col-md-7 col-xs-7 dataInput">
                                 <label htmlFor="email">Email address</label>
                                 <input type="email" className="form-control" name="email" id="eName" placeholder="e-mail" />
                             </div>
                         </div>
-                    <div className="form-group formProfile">
-                        <i className="fa fa-check-circle col-xs-3" aria-hidden="true" />
-                        <div className="col-xs-8">
-                            <label className="form-check-label">
-                                <input className="form-check-input" id="agreeUser" type="checkbox" onChange={onInputChange}/>
-                                I agree to Lyft's <a href="lyft.com"> Terms of Service</a>
-                            </label>
-
-                            {
-                                this.state.goFordward ?     
-                                    <div className="form-group">
-                                        <i className="fa fa-check-circle col-xs-3" aria-hidden="true" />
-                                        <div className="col-xs-8">
-                                            <NavLink to={"/map"}>
-                                                <button type="submit" className="btn btn-default disabled">NEXT</button>
-                                            </NavLink>   
-                                        </div>
-                                    </div>
-                                    :
-                                    <div className="form-group">
-                                        <i className="fa fa-check-circle col-xs-3" aria-hidden="true" />
-                                        <div className="col-xs-8">
-                                            <button type="submit" className="btn btn-default disabled">NEXT</button>
-                                        </div>
-                                    </div>
-                                   
-                            }
-                       </div>
+                        </form>
+                        </center>
                     </div>
-                    </form></div>        
+					<label className="form-check-label">
+						<input className="form-check-input" id="agreeUser" type="checkbox" onChange={onInputChange}/>
+						I agree to Lyft's <a href="lyft.com"> Terms of Service</a>
+					</label>
+
+					{
+						this.state.goFordward ?
+							<NavLink
+								to={"/map"}>
+                                <button
+								className="btn btn-lg btn-block btn-lyft">Next</button>
+                            </NavLink>
+							:
+							<button
+								className="btn btn-lg btn-block btn-lyft disabled">Next</button>
+
+					}
+
+				</section>
 			</div>
 
 		);
